0de580c272a0f22250c97a8e1c8501ce0afa3328
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 18:08:42 +0200

    Formatting code.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure	"1.9.0"]
-         								[org.vladimir/mongo-lib "0.1.0"]
-         								[org.vladimir/session-lib "0.1.0"]
-         								[org.vladimir/language-lib "0.1.0"]
-         								[org.vladimir/dao-lib "0.1.0"]
-         								[org.vladimir/ajax-lib "0.1.0"]
-         								[org.vladimir/utils-lib "0.1.0"]
-         								]
+                 [org.vladimir/mongo-lib "0.1.0"]
+                 [org.vladimir/session-lib "0.1.0"]
+                 [org.vladimir/language-lib "0.1.0"]
+                 [org.vladimir/dao-lib "0.1.0"]
+                 [org.vladimir/ajax-lib "0.1.0"]
+                 [org.vladimir/utils-lib "0.1.0"]
+                 ]
   :source-paths ["src/clj"])
 
