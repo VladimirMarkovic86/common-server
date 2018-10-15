09afa52d8881c6f8a963aef15cbf130a6488494a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:44:27 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,16 +1,19 @@
-(defproject org.vladimir/common-server "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.1.0"
   :description "Common server"
-  :url "http://gitlab:1610/VladimirMarkovic86/common-server"
+  :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/db-lib "0.1.0"]
-                 [org.vladimir/session-lib "0.1.0"]
-                 [org.vladimir/language-lib "0.1.0"]
-                 [org.vladimir/dao-lib "0.1.0"]
-                 [org.vladimir/ajax-lib "0.1.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
-                 [org.vladimir/common-middle "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/db-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/clj"])
 
