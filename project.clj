2619694483b699a240a737021d02f53fa52ae30a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-11-01 11:55:10 +0100

    Updated dao-lib version and current project version also.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-server "0.3.1"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.3.2"
   :description "Common server"
   :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.0"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.0"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.2.0"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.0"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.1.0"]
