b923c56ff153a01540dbe4c835601d538c9d9cb0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-18 16:08:40 +0100

    Task63: Fixed optimization advanced to work with new code, compiling production version fixed

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject org.clojars.vladimirmarkovic86/common-server "0.3.19"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.3.20"
   :description "Common server"
   :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.11"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.14"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.15"]
                  [org.clojars.vladimirmarkovic86/dao-lib "0.3.9"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.7"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.6"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  ]
