7f0db85c28900de03915f60541ef87f039fde724
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-21 12:59:55 +0100

    Task 63: Implemented tests for dao-lib.core namespace.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-server "0.3.22"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.3.23"
   :description "Common server"
   :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.5"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.14"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.17"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.10"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.18"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.11"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.6"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
