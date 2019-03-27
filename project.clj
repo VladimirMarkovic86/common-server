f4a44810559d2103663c039e545452039c960321
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-27 11:48:52 +0100

    Task 68: Implemented three different scrapers which can be adapted to clients requests.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-server "0.3.23"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.3.24"
   :description "Common server"
   :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
@@ -6,10 +6,10 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.5"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.14"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.18"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.11"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.19"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.12"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.8"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.6"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.7"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.5"]
                  ]
 
