6ba485a63a1b065b6c09eae54de51174b5c45a95
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-05-31 16:44:08 +0200

    Task 63: Implemented tests for language-lib.core.clj and language-lib.core.cljs namespaces.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/common-server "0.3.34"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.3.35"
   :description "Common server"
   :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.8"]
                  [org.clojars.vladimirmarkovic86/session-lib "0.2.19"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.26"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.27"]
                  [org.clojars.vladimirmarkovic86/dao-lib "0.3.19"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.9"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.9"]
