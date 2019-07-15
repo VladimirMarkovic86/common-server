c0c26bb59ae3affa232c3810c52eb2cc333b0171
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:53:27 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject org.clojars.vladimirmarkovic86/common-server "0.3.43"
+(defproject org.clojars.vladimirmarkovic86/common-server "0.3.44"
   :description "Common server"
   :url "http://github.com/VladimirMarkovic86/common-server"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.34"]
-                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.26"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
-                 [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.7"]
-                 [org.clojars.vladimirmarkovic86/mail-lib "0.1.1"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.35"]
+                 [org.clojars.vladimirmarkovic86/dao-lib "0.3.27"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
+                 [org.clojars.vladimirmarkovic86/common-middle "0.2.10"]
+                 [org.clojars.vladimirmarkovic86/pdflatex-lib "0.1.8"]
+                 [org.clojars.vladimirmarkovic86/mail-lib "0.1.2"]
                  ]
 
   :min-lein-version "2.0.0"
@@ -20,5 +20,7 @@
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   
-  :jar-exclusions [#"^reports/"])
+  :jar-exclusions [#"^reports/"
+                   #"README.md$"
+                   #"LICENSE$"])
 
